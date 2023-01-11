9462bcdaebfd557efe6120c8428885db7a997f8d
PARKGYUNAM <qkr5318@gmail.com>
2023-01-11 17:09:47 +0900

    Revert "2023-01-11 jsp 네이버 API , 게시판, 방명록"
    This reverts commit 4c990ee592348707b524cfbd9265705b5956c21c.

--- BOARD_SIGNUP/src/auth/service/LoginFailException.java
+++
@@ -1,7 +0,0 @@
-package auth.service;
-
-// 아이디와 암호가 잘못된 경우
-// LoginFailException을 발생시킨다.
-public class LoginFailException extends RuntimeException{
-
-}
--- BOARD_SIGNUP/src/member/service/MemberNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package member.service;
-
-// 암호를 변경할 회원 데이터가 존재하지 않는 경우 발생하는 예외 처리 클래스 작성
-public class MemberNotFoundException extends RuntimeException {
-
-}
